9be00b27ce38bf84c6ee5e42acac18b58d7449db
OleynikDima <57680550+OleynikDima@users.noreply.github.com>
2020-08-04 22:31:34 +0300

    Updates

---
+++ precache-manifest.bca2781c9048911a66169de6900eedff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75fac1e7886a211c3c03d7d5c964e718",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "4ed712d3d4f97100c3af",
+    "url": "/goit-react-hw-02-feedback/static/css/main.92c39e1a.chunk.css"
+  },
+  {
+    "revision": "6c49cd26651a78b87686",
+    "url": "/goit-react-hw-02-feedback/static/js/2.f677a506.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.f677a506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ed712d3d4f97100c3af",
+    "url": "/goit-react-hw-02-feedback/static/js/main.c7643d01.chunk.js"
+  },
+  {
+    "revision": "25083be5abe132dedb91",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.e7f9aa6f.js"
+  }
+]);
